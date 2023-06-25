c96b07cb8edf9cc23ee4ef0552a9f7ae23f24fdb
Lakmilak <Tsvetstas30@gmail.com>
2023-06-25 18:12:05 +1000

    some-changes

--- src/main/java/org/jenjetsu/com/finch/HelloController.java
+++ src/main/java/org/jenjetsu/com/finch/HelloController.java
@@ -19,161 +19,184 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import org.jenjetsu.com.finch.library.Finch;
 
-import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
-
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
 public class HelloController implements Initializable{
 
     private final URI BUTTON_PRESSED_IMAGE_URI = Paths.get("src/main/resources/image/arrow.png").toUri();
     private final URI BUTTON_RELEASED_IMAGE_URI = Paths.get("src/main/resources/image/arrow_tint.png").toUri();
-    private final int MAX_FINCH_VISION_DISTANCE_SM = 250;
-    private AtomicReference<KeyCode> CURRENT_PRESSED_KEY;
+    private final int MAX_FINCH_VISION_DISTANCE_SM = 200;
+    private double MAX_SPEED = 30;
     private Timer finchUpdater = new Timer();
+    private ConcurrentLinkedDeque<Character> message;
+    private boolean isAutopilotActive;
+    private int[] color = new int[3];
+    private int[] predColor = new int[]{-1, -1, -1};
+    private boolean isBeepSignal;
+
     @FXML private Slider redRGBSlider;
     @FXML private Slider greenRGBSlider;
     @FXML private Slider blueRGBSlider;
+    @FXML private Slider speedSlider;
     @FXML private Pane mainPane;
     @FXML private Canvas finchScreen;
-    @FXML private Button upButton;
-    @FXML private Button downButton;
-    @FXML private Button leftButton;
-    @FXML private Button rightButton;
+
+    private ConcurrentLinkedQueue<KeyCode> keys;
 
     private final Finch finch;
-
-    private int red;
-    private int green;
-    private int blue;
+    private Timer finchAutopilotTimer = new Timer();
+    private long letterTimer;
 
     public HelloController() {
         finch = new Finch("A");
-        CURRENT_PRESSED_KEY = new AtomicReference<>(null);
-        red = 100;
-        green = 100;
-        blue = 100;
+        keys = new ConcurrentLinkedQueue<>();
+        color[0] = 0;
+        color[1] = 0;
+        color[2] = 0;
+        letterTimer = 0l;
+        message = new ConcurrentLinkedDeque<>();
     }
 
     public void initialize(URL url, ResourceBundle rb) {
-        redRGBSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
-            if(!newValue.equals(oldValue)) {
-                red = newValue.intValue();
-                changeFinchColor();
-            }
-        });
-        greenRGBSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
-            if(!newValue.equals(oldValue)) {
-                green = newValue.intValue();
-                changeFinchColor();
-            }
-        });
-        blueRGBSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
-            if(!newValue.equals(oldValue)){
-                blue = newValue.intValue();
-                changeFinchColor();
-            }
+        redRGBSlider.valueProperty().addListener((observable, oldValue, newValue) -> color[0] = newValue.intValue());
+        greenRGBSlider.valueProperty().addListener((observable, oldValue, newValue) -> color[1] = newValue.intValue());
+        blueRGBSlider.valueProperty().addListener((observable, oldValue, newValue) -> color[2] = newValue.intValue());
+        speedSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            MAX_SPEED = newValue.doubleValue();
+            Text text = (Text) mainPane.getScene().lookup("#speedText");
+            text.setText(String.format("SPEED: %d", (int) MAX_SPEED));
         });
         GraphicsContext gc = finchScreen.getGraphicsContext2D();
         gc.setFill(Color.BLACK);
         gc.fillRect(0, 0, finchScreen.getWidth(), finchScreen.getHeight());
-        changeFinchColor();
-        upButton.setPadding(Insets.EMPTY);
-        downButton.setPadding(Insets.EMPTY);
-        leftButton.setPadding(Insets.EMPTY);
-        rightButton.setPadding(Insets.EMPTY);
-        finchUpdater.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                changeAllButtonsImage();
-                changeDistance(finch.getDistance());
-                updateFinchScreen(finch.getDistance());
-                changeLight(finch.getLight("L"));
-                rotateCompassArrow(finch.getCompass());
-                if(CURRENT_PRESSED_KEY.get() != null) {
-                    String keyCode = CURRENT_PRESSED_KEY.get().getName();
-                    changeArrowImage(CURRENT_PRESSED_KEY.get().getName(), false);
-                    moveFinch(keyCode, false);
-                } else {
-                    moveFinch("", true);
-                }
-            }
-        }, 1000, 30);
+        mainPane.lookupAll(".moveButton").forEach((node) -> ((Button) node).setPadding(Insets.EMPTY));
+        finchUpdater.schedule(createCustomControlTask(), 1000, 30);
     }
 
     public void close() {
         System.out.println("stopped");
-        finchUpdater.cancel();
-        finchUpdater.purge();
+        if(isAutopilotActive) {
+            finchAutopilotTimer.cancel();
+            finchAutopilotTimer.purge();
+        } else  {
+            finchUpdater.cancel();
+            finchUpdater.purge();
+        }
         finch.setBeak(0, 0, 0);
         finch.stop();
         finch.disconnect();
-    }
-
-    private synchronized Finch getFinch() {
-        return finch;
     }
 
     // Управление Finchом
     @FXML
     private void handleKeyPressed(KeyEvent ke) {
         if(ke.getCode() == KeyCode.W || ke.getCode() == KeyCode.S || ke.getCode() == KeyCode.D || ke.getCode() == KeyCode.A) {
-            CURRENT_PRESSED_KEY.set(ke.getCode());
+            if(!keys.contains(ke.getCode())) {
+                keys.add(ke.getCode());
+            }
         }
     }
 
     @FXML
     private void releaseKeyPress(KeyEvent ke) {
         if(ke.getCode() == KeyCode.W || ke.getCode() == KeyCode.S || ke.getCode() == KeyCode.D || ke.getCode() == KeyCode.A) {
-            CURRENT_PRESSED_KEY.set(null);
+            if(keys.contains(ke.getCode())) {
+                keys.remove(ke.getCode());
+            }
         }
     }
 
     @FXML
     private void releaseArrowButtonPress(MouseEvent me) {
         String key = Converter.convertButtonIdToKey(((Button) me.getSource()).getId());
-        moveFinch(key, true);
-        changeArrowImage(key, true);
+        changeArrowImage();
         updateFinchScreen(finch.getDistance());
     }
 
     @FXML
     private void handleArrowButtonPress(MouseEvent me) {
         String key = Converter.convertButtonIdToKey(((Button) me.getSource()).getId());
-        moveFinch(key, false);
-        changeArrowImage(key, false);
+        changeArrowImage();
         updateFinchScreen(finch.getDistance());
     }
 
+    @FXML
+    private void showMessageOnDisplay() {
+        if(message.isEmpty()) {
+            TextField field = (TextField) mainPane.getScene().lookup("#messageField");
+            char[] characters = field.getText().toCharArray();
+            for (char ch : characters) {
+                message.add(ch);
+            }
+        }
+    }
+
+    @FXML
+    private void beep() {
+        isBeepSignal = true;
+    }
+
     private void changeFinchColor() {
-        finch.setBeak(red, green, blue);
+        if(!Arrays.equals(color, predColor)) {
+            predColor[0] = color[0];
+            predColor[1] = color[1];
+            predColor[2] = color[2];
+            ((Text) mainPane.getScene().lookup("#redText")).setText("RED: " + color[0]);
+            ((Text) mainPane.getScene().lookup("#greenText")).setText("GREEN: " + color[1]);
+            ((Text) mainPane.getScene().lookup("#blueText")).setText("BLUE: " + color[2]);
+            finch.setBeak(predColor[0], predColor[1], predColor[2]);
+        }
     }
 
     // Подуправление Finchом
-    private void moveFinch(String key, boolean isStop) {
-        if(!isStop) {
-            if(key.equals("W")) {
-                finch.setMotors(100, 100);
-            } else if (key.equals("S")) {
-                finch.setMotors(-100, -100);
-            } else if (key.equals("A")) {
-                finch.setMotors(-50, 50);
-            } else if (key.equals("D")) {
-                finch.setMotors(50, -50);
+    private void moveFinch() {
+        double HALF_SPEED = MAX_SPEED / 2;
+        if(keys.size() == 1) {
+            KeyCode code = keys.peek();
+            switch (code.getName()) {
+                case "W" -> finch.setMotors(MAX_SPEED, MAX_SPEED);
+                case "S" -> finch.setMotors(-MAX_SPEED, -MAX_SPEED);
+                case "A" -> finch.setMotors(-HALF_SPEED, HALF_SPEED);
+                case "D" -> finch.setMotors(HALF_SPEED, -HALF_SPEED);
+            }
+        } else if (keys.size() == 2) {
+            if(keys.contains(KeyCode.W) && keys.contains(KeyCode.D)) {
+                finch.setMotors(MAX_SPEED, HALF_SPEED);
+            } else if(keys.contains(KeyCode.W) && keys.contains(KeyCode.A)) {
+                finch.setMotors(HALF_SPEED, MAX_SPEED);
+            } else if (keys.contains(KeyCode.S) && keys.contains(KeyCode.D)) {
+                finch.setMotors(-MAX_SPEED, -HALF_SPEED);
+            } else {
+                finch.setMotors(-HALF_SPEED, -MAX_SPEED);
             }
         } else {
             finch.setMotors(0, 0);
         }
     }
 
-    private void changeArrowImage(String key, boolean isStop) {
-        URI imageUri = isStop ? BUTTON_PRESSED_IMAGE_URI : BUTTON_RELEASED_IMAGE_URI;
-        Button pressedButton = (Button) mainPane.getScene().lookup("#"+Converter.convertKeyToButtonId(key));
-        ImageView img = (ImageView) pressedButton.getChildrenUnmodifiable().get(0);
-        if(!img.getImage().getUrl().equals(imageUri)) {
-            img.setImage(new Image(imageUri.toString()));
+    private void changeArrowImage() {
+        for(KeyCode key : keys) {
+            String buttonName = Converter.convertKeyToButtonId(key.getName());
+            Button pressedButton = (Button) mainPane.getScene().lookup("#"+buttonName);
+            ImageView img = (ImageView) pressedButton.getChildrenUnmodifiable().get(0);
+            img.setImage(new Image(BUTTON_RELEASED_IMAGE_URI.toString()));
+        }
+    }
+
+    private void changeAllButtonsImage() {
+        Set<Node> buttons = mainPane.lookupAll(".moveButton");
+        for(Node node : buttons) {
+            Button button = (Button) node;
+            String keyName = Converter.convertButtonIdToKey(button.getId());
+            if(keys.contains(KeyCode.getKeyCode(keyName))) continue;
+            ImageView view = (ImageView) button.getChildrenUnmodifiable().get(0);
+            if(view.getImage().getUrl().toString().endsWith("arrow_tint.png")) {
+                view.setImage(new Image(BUTTON_PRESSED_IMAGE_URI.toString()));
+            }
         }
     }
 
@@ -182,15 +205,19 @@ public class HelloController implements Initializable{
         distanceText.setText(distance + " sm");
     }
 
-    @FXML
-    private void showMessageOnDisplay() {
-        TextField field = (TextField) mainPane.getScene().lookup("#messageField");
-        String message = field.getText();
-        for(Character ch : message.toCharArray()) {
-            int[] display = RussionLetterConverter.convertToDisplay(ch);
-            finch.setDisplay(display);
-            finch.pause(0.8);
-            finch.setDisplay(RussionLetterConverter.STANDART_DISPLAY);
+    private void showOnDisplay() {
+        if(!message.isEmpty()) {
+            if(letterTimer < System.currentTimeMillis()) {
+                letterTimer = System.currentTimeMillis() + 1000l;
+                char letter = message.pop();
+                if((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z') || (letter >= '0' && letter <= '9') || letter == ' ') {
+                    finch.print(""+letter);
+                } else {
+                    finch.setDisplay(RussionLetterConverter.STANDART_DISPLAY);
+                    int[] display = RussionLetterConverter.convertToDisplay(letter);
+                    finch.setDisplay(display);
+                }
+            }
         }
     }
 
@@ -204,28 +231,20 @@ public class HelloController implements Initializable{
         compassArrow.setRotate(degAngle);
     }
 
-    private void changeAllButtonsImage() {
-        Set<Node> buttons = mainPane.lookupAll(".moveButton");
-        for(Node node : buttons) {
-            ImageView view = (ImageView) ((Button) node).getChildrenUnmodifiable().get(0);
-            if(view.getImage().getUrl().toString().endsWith("arrow_tint.png")) {
-                view.setImage(new Image(BUTTON_PRESSED_IMAGE_URI.toString()));
-            }
-        }
-    }
-
     private void updateFinchScreen(int distance) {
         GraphicsContext gc = finchScreen.getGraphicsContext2D();
         gc.setFill(Color.BLACK);
         gc.fillRect(0, 0, finchScreen.getWidth(), finchScreen.getHeight());
         if ((distance <= MAX_FINCH_VISION_DISTANCE_SM)) {
             gc.setStroke(Color.RED);
+            if(distance < 1) distance = 1;
             double cx = finchScreen.getWidth() / 2.0;
             double cy = finchScreen.getHeight() / 2.0;
-            double x1 = (cx - 50) / distance;
-            double y1 = (cy - 50) / distance;
-            double x2 = (cx + 50) / distance;
-            double y2 = (cy + 50) / distance;
+            double d = 30;
+            double x1 = cx - d * cx / (d + distance);
+            double y1 = cy - d * cy / (d + distance);
+            double x2 = cx + d * cx / (d + distance);
+            double y2 = cy + d * cy / (d + distance);
             gc.beginPath();
             gc.moveTo(x1, y1);
             gc.lineTo(x2, y1);
@@ -236,6 +255,78 @@ public class HelloController implements Initializable{
         }
     }
 
+    @FXML
+    private void activateAutoNavigation() {
+        if(!isAutopilotActive) {
+            finchUpdater.cancel();
+            finchUpdater.purge();
+            finchAutopilotTimer = new Timer();
+            finchAutopilotTimer.schedule(createAutopilotTask(), 1000, 20);
+            isAutopilotActive = !isAutopilotActive;
+        } else {
+            finchAutopilotTimer.cancel();
+            finchAutopilotTimer.purge();
+            finchUpdater = new Timer();
+            finchUpdater.schedule(createCustomControlTask(), 1000, 20);
+            isAutopilotActive = !isAutopilotActive;
+        }
+    }
+
+    private TimerTask createAutopilotTask() {
+        return new TimerTask() {
+            @Override
+            public void run() {
+                if(isBeepSignal) {
+                    finch.playNote(50, 2);
+                    isBeepSignal = false;
+                }
+                int distance = finch.getDistance();
+                rotateCompassArrow(finch.getCompass());
+                changeFinchColor();
+                updateFinchScreen(distance);
+                changeDistance(distance);
+                if(distance < 20) {
+                    finch.setMove("B", 30, 50);
+                } else if(distance < 40) {
+                    finch.setTurn("L", 30, 50);
+                    int left = finch.getDistance();
+                    updateFinchScreen(left);
+                    finch.setTurn("R", 60, 50);
+                    int right = finch.getDistance();
+                    updateFinchScreen(right);
+                    finch.setTurn("L", 30, 50);
+                    if(left > right){
+                        finch.setTurn("L", 45, 50);
+                    } else {
+                        finch.setTurn("R", 45, 50);
+                    }
+                } else {
+                    finch.setMove("F", 10, 50);
+                }
+            }
+        };
+    }
+
+    private TimerTask createCustomControlTask() {
+        return new TimerTask() {
+            @Override
+            public void run() {
+                if(isBeepSignal) {
+                    finch.playNote(50, 2);
+                    isBeepSignal = false;
+                }
+                changeAllButtonsImage();
+                showOnDisplay();
+                changeFinchColor();
+                changeDistance(finch.getDistance());
+                updateFinchScreen(finch.getDistance());
+                changeLight(finch.getLight("L"));
+                rotateCompassArrow(finch.getCompass());
+                moveFinch();
+                changeArrowImage();
+            }
+        };
+    }
 }
 
 // Конвертеры для кнопок
@@ -263,9 +354,9 @@ class Converter{
                 break;
             case "downButton"  : retVal = "S";
                 break;
-            case "leftButton" : retVal = "D";
+            case "leftButton" : retVal = "A";
                 break;
-            case "rightButton"  : retVal = "A";
+            case "rightButton"  : retVal = "D";
                 break;
         };
         return retVal;
